ea52b7e36911efb8d35fee6deed3647ac590360a
1176277624@qq.com <1176277624@qq.com>
2018-06-25 20:44:59 +0800

    'AddAdminUserController'

---
+++ PalmRent.AdminWeb/Models/AdminUserAddViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PalmRent.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PalmRent.AdminWeb.Models
+{
+    public class AdminUserAddViewModel
+    {
+        public CityDTO[] Cities { get; set; }
+        public RoleDTO[] Roles { get; set; }
+    }
+}
---
+++ PalmRent.AdminWeb/Models/AdminUserEditViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PalmRent.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PalmRent.AdminWeb.Models
+{
+    public class AdminUserEditViewModel
+    {
+        public AdminUserDTO AdminUser { get; set; }
+        public CityDTO[] Cities { get; set; }
+        public RoleDTO[] Roles { get; set; }
+
+        /// <summary>
+        /// 当前用户拥有的角色id
+        /// </summary>
+        public long[] UserRoleIds { get; set; }
+    }
+}
