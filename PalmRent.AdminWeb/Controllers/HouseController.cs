8560f9da23824303d5c86795197e12a0d963e23a
1176277624@qq.com <1176277624@qq.com>
2018-07-01 16:38:34 +0800

    '图片上传'

--- PalmRent.AdminWeb/Controllers/HouseController.cs
+++ PalmRent.AdminWeb/Controllers/HouseController.cs
@@ -1,10 +1,14 @@
-﻿using PalmRent.AdminWeb.App_Start;
+﻿using CodeCarvings.Piczard;
+using CodeCarvings.Piczard.Filters.Watermarks;
+using PalmRent.AdminWeb.App_Start;
 using PalmRent.AdminWeb.Models;
+using PalmRent.Common;
 using PalmRent.CommonMVC;
 using PalmRent.DTO;
 using PalmRent.IService;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -118,20 +122,20 @@ namespace PalmRent.AdminWeb.Controllers
             return Json(new AjaxResult { Status = "ok" });
         }
 
-        //private void CreateStaticPage(long houseId)
-        //{
-        //    var house = houseService.GetById(houseId);
-        //    var pics = houseService.GetPics(houseId);
-        //    var attachments = attService.GetAttachments(houseId);
+        private void CreateStaticPage(long houseId)
+        {
+            var house = houseService.GetById(houseId);
+            var pics = houseService.GetPics(houseId);
+            var attachments = attService.GetAttachments(houseId);
 
-        //    HouseIndexViewModel model = new HouseIndexViewModel();
-        //    model.House = house;
-        //    model.Pics = pics;
-        //    model.Attachments = attachments;
-        //    string html = MVCHelper.RenderViewToString(this.ControllerContext, @"~/Views/House/StaticIndex.cshtml",
-        //        model);
-        //    System.IO.File.WriteAllText(@"E:\快盘\NextBig\NET课程\NET掌上租\上课代码\ZSZ\ZSZ.FrontWeb\" + houseId + ".html", html);
-        //}
+            HouseIndexViewModel model = new HouseIndexViewModel();
+            model.House = house;
+            model.Pics = pics;
+            model.Attachments = attachments;
+            string html = MVCHelper.RenderViewToString(this.ControllerContext, @"~/Views/House/StaticIndex.cshtml",
+                model);
+            System.IO.File.WriteAllText(@"E:\快盘\NextBig\NET课程\NET掌上租\上课代码\ZSZ\ZSZ.FrontWeb\" + houseId + ".html", html);
+        }
 
         [CheckPermission("House.Edit")]
         [HttpGet]
@@ -191,6 +195,56 @@ namespace PalmRent.AdminWeb.Controllers
 
             //CreateStaticPage(model.Id);//编辑房源的时候重新生成静态页面
             return Json(new AjaxResult { Status = "ok" });
+        }
+
+        public ActionResult PicUpload(int houseId)
+        {
+            return View(houseId);
+        }
+
+        public ActionResult UploadPic(int houseId, HttpPostedFileBase file)
+        {
+            /*
+            if (houseId < 5)
+            {
+                return Json(new AjaxResult { Status = "error", ErrorMsg = "id必须大于5" });
+            }*/
+            //month月，minute
+            string md5 = CommonHelper.CalcMD5(file.InputStream);
+            string ext = Path.GetExtension(file.FileName);
+            string path = "/upload/" + DateTime.Now.ToString("yyyy/MM/dd") + "/" + md5 + ext;// /upload/2017/07/07/afadsfa.jpg
+            string thumbPath = "/upload/" + DateTime.Now.ToString("yyyy/MM/dd") + "/" + md5 + "_thumb" + ext;
+            string fullPath = HttpContext.Server.MapPath("~" + path);//d://22/upload/2017/07/07/afadsfa.jpg
+            string thumbFullPath = HttpContext.Server.MapPath("~" + thumbPath);
+            new FileInfo(fullPath).Directory.Create();//尝试创建可能不存在的文件夹
+
+            file.InputStream.Position = 0;//指针复位
+            //file.SaveAs(fullPath);//SaveAs("d:/1.jpg");
+            //缩略图
+            ImageProcessingJob jobThumb = new ImageProcessingJob();
+            jobThumb.Filters.Add(new FixedResizeConstraint(200, 200));//缩略图尺寸200*200
+            jobThumb.SaveProcessedImageToFileSystem(file.InputStream, thumbFullPath);
+
+            file.InputStream.Position = 0;//指针复位
+
+            //水印
+            ImageWatermark imgWatermark =
+                new ImageWatermark(HttpContext.Server.MapPath("~/images/watermark.jpg"));
+            imgWatermark.ContentAlignment = System.Drawing.ContentAlignment.BottomRight;//水印位置
+            imgWatermark.Alpha = 50;//透明度，需要水印图片是背景透明的png图片
+            ImageProcessingJob jobNormal = new ImageProcessingJob();
+            jobNormal.Filters.Add(imgWatermark);//添加水印
+            jobNormal.Filters.Add(new FixedResizeConstraint(600, 600));
+            jobNormal.SaveProcessedImageToFileSystem(file.InputStream, fullPath);
+
+            houseService.AddNewHousePic(new HousePicDTO { HouseId = houseId, Url = path, ThumbUrl = thumbPath });
+
+           // CreateStaticPage(houseId);//上传了新图片或者删除图片都要重新生成html页面
+
+            return Json(new AjaxResult
+            {
+                Status = "ok"
+            });
         }
     }
 }
